7c871492e23afa29aa788eb9758d39eb1512ae46
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-03-01 16:54:20 -0700

    Controller Video

---
+++ Controllers/PurchaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View(new Purchase());
+        }
+    }
+}
