af0cbe647967a54dbc5b61fbb9c0da30d4da439b
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-02-28 19:09:39 -0700

    Session video

---
+++ Models/SessionCart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using BookApp.Infrastructure;
+using Microsoft.AspNetCore.Http;
+
+namespace BookApp.Models
+{
+    public class SessionCart : Cart
+    {
+        [JsonIgnore]
+        public ISession Session { get; set; }
+
+        public override void AddItem(Book book, int qty)
+        {
+            base.AddItem(book, qty);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void RemoveItem(Book book)
+        {
+            base.RemoveItem(book);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void ClearCart()
+        {
+            base.ClearCart();
+            Session.Remove("Cart");
+        }
+    }
+}
