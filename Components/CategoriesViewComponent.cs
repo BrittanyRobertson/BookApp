bef3833e66618fd0a503a63949a67b2b37c5e0d0
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-02-28 18:17:02 -0700

    Initial commit

---
+++ Components/CategoriesViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using BookApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private IBookstoreRepository repo { get; set; }
+
+        public CategoriesViewComponent(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["Category"];
+
+            var categories = repo.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(categories);
+        }
+    }
+}
---
+++ Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BookApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Models
+{
+    public class BooksViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
