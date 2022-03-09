adc9d08641c9ceafa92cf5716580165c7f38505f
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-03-09 15:54:06 -0700

    made every field required

--- Models/Book.cs
+++ Models/Book.cs
@@ -15,12 +15,19 @@ namespace BookApp.Models
         public long BookId { get; set; }
         [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Required]
         public string Publisher { get; set; }
+        [Required]
         public string Isbn { get; set; }
+        [Required]
         public string Classification { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public long PageCount { get; set; }
+        [Required]
         public double Price { get; set; }
     }
 }
